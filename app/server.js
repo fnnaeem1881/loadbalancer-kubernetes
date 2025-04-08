0685b4a62b27525e248df5bb421619af76093ee5
Mehedi Hasan <mehedinaeem66@gmail.com>
2025-04-08 21:50:55 +0600

    done

---
+++ app/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const os = require('os');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send(`Hello from ${os.hostname()}!`);
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
